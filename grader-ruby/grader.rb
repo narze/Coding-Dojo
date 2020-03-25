4ad37621bd62d481f968ae92a1767a33cc9db403
Manassarn Manoonchai <manassarn@gmail.com>
2020-03-25 22:18:07 +0700

    :sparkles: Add new method without using switch case

--- grader-ruby/grader.rb
+++ grader-ruby/grader.rb
@@ -2,6 +2,18 @@ class Grader
   def self.call(score)
     raise ArgumentError, "Non integer score" unless score.kind_of?(Integer)
 
+    scores = [101, 91, 81, 71, 61, 0] << score
+
+    grade = "xABCDFx"[scores.sort.reverse.find_index(score)]
+
+    raise ArgumentError, "Out of bounds" if grade == "x"
+
+    grade
+  end
+
+  def self.call_with_case(score)
+    raise ArgumentError, "Non integer score" unless score.kind_of?(Integer)
+
     case score
     when 91..100
       "A"
